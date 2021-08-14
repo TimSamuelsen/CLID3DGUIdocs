a5fa21503950e95d51311495687fc254c09fedcf
LAPTOP-4QRN01E9\le54t <tim.samuelsen@gmail.com>
2021-08-14 12:36:00 -0700

    adding breathe project

--- docs/conf.py
+++ docs/conf.py
@@ -289,3 +289,7 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+# -- Extension configuration -------------------------------------------------
+
+breathe_projects = { 'src': '_doxygen/xml' }
